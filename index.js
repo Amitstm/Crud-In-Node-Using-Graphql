178d2db5a6d68f676f7837f23f14afd4608b5fbb
Amitstm <amitkumarmahto76@gmail.com>
2021-07-23 15:19:08 +0530

    fix db error

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import schema from './schema';
 const app = express();
 const PORT = 3000;
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Graph Ql');
+mongoose.connect('mongodb://localhost/Graph-Ql',{ useNewUrlParser: true}, {  useUnifiedTopology : true});
 app.get('/', (req, res) => {
     res.json({
         msg: 'Welcome to GraphQL'
